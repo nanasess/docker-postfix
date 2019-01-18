da879b5ce745fb6210c011c43da724e558bb9c80
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-01-18 09:33:12 +0900

    Setting variable `$smtp_user` will enable SMTP-AUTH

--- assets/install.sh
+++ assets/install.sh
@@ -40,24 +40,23 @@ postconf -e "mynetworks=$mynetworks"
 # Cyrus-SASL support for authentication of mail clients.
 ############
 # /etc/postfix/main.cf
-# postconf -e smtpd_sasl_auth_enable=yes
-# postconf -e broken_sasl_auth_clients=yes
-# postconf -e smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination
-# # smtpd.conf
-# cat >> /etc/postfix/sasl/smtpd.conf <<EOF
-# pwcheck_method: auxprop
-# auxprop_plugin: sasldb
-# mech_list: PLAIN LOGIN CRAM-MD5 DIGEST-MD5 NTLM
-# EOF
-# # sasldb2
-# echo $smtp_user | tr , \\n > /tmp/passwd
-# while IFS=':' read -r _user _pwd; do
-#   echo $_pwd | saslpasswd2 -p -c -u $maildomain $_user
-# done < /tmp/passwd
-# chown postfix.sasl /etc/sasldb2
-
-# Add dockerhost
-postconf -e "mynetworks=172.17.0.0/24 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128"
+if [[ -n $smtp_user ]]; then
+    postconf -e smtpd_sasl_auth_enable=yes
+    postconf -e broken_sasl_auth_clients=yes
+    postconf -e smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination
+    # smtpd.conf
+    cat >> /etc/postfix/sasl/smtpd.conf <<EOF
+pwcheck_method: auxprop
+auxprop_plugin: sasldb
+mech_list: PLAIN LOGIN CRAM-MD5 DIGEST-MD5 NTLM
+EOF
+    # sasldb2
+    echo $smtp_user | tr , \\n > /tmp/passwd
+    while IFS=':' read -r _user _pwd; do
+        echo $_pwd | saslpasswd2 -p -c -u $maildomain $_user
+    done < /tmp/passwd
+    chown postfix.sasl /etc/sasldb2
+fi
 
 ############
 # relayhost
